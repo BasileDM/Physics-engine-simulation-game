c6e7ce7ce4ecc0390387cf109f04695f3bd413c5
BasileDM <125025171+BasileDM@users.noreply.github.com>
2024-02-03 11:50:54 +0100

    add colliding status flag

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Particle } from "./scripts/classes/Particle.js";
 
 // Variables
-let particles = [];
+let particles = []; // Array of all Particle object instances
 
 // Functions
 function createParticle(event) {
@@ -13,34 +13,40 @@ function createParticle(event) {
 }
 
 function mainLoop() {
+    // Iterating through all the particles to update and render them
     for(let i = 0; i < particles.length; i++) {
         let currentParticle = particles[i];
-        currentParticle.update();
-        currentParticle.render();
 
+        // Collision check
         for (let j = 0; j < particles.length; j++) {
             // i != j to avoid collision with itself
             if (i != j) {
                 let checkedParticle = particles[j];
 
-                // Distance check from stackoverflow https://stackoverflow.com/questions/45439503/distance-function-returning-the-wrong-thing
+                // Pythagorian distance check from stackoverflow https://stackoverflow.com/questions/45439503/distance-function-returning-the-wrong-thing
                 let distanceX = currentParticle.positionX - checkedParticle.positionX;
                 let distanceY = currentParticle.positionY - checkedParticle.positionY;
                 let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
     
-                if (distance < 10) {
-                    console.log(`${i} and ${j} are colliding.`);
-                    console.log(particles);
-                    currentParticle.velocity -= 0.15;
+                if (distance < 12) {
+                    // console.log(`${i} and ${j} are colliding.`);
+                    // console.log(particles);
+                    currentParticle.setColliding(true);
+                    currentParticle.velocity = 0;
                 }
             }
-        }   
+        }
+        currentParticle.update();
         
-        if (currentParticle.positionY > 500) {
+        // Destroy a particle if it falls below the playground area
+        if (currentParticle.positionY > 3*window.innerHeight/4) {
             currentParticle.destroy();
             particles.splice(i, 1);
             i--;
         }
+
+        // Render the particle after all the checks
+        currentParticle.render();
     }
     requestAnimationFrame(mainLoop);
 }
--- scripts/classes/Particle.js
+++ scripts/classes/Particle.js
@@ -1,3 +1,5 @@
+const gravity = 0.05;
+
 export class Particle {
     constructor(positionXParam, positionYParam) {
         this.element = document.createElement("div");
@@ -7,33 +9,42 @@ export class Particle {
         this.height = "10px"
         this.color = "red";
         this.velocity = 0;
+        this.isColliding = false;
     }
 
     getElement() {
         return this.element;
     }
 
+    setColliding(isCollidingParam) {
+        this.isColliding = isCollidingParam;
+    }
+
     spawn() {
-        let newParticle = this.getElement();
-        // newParticle.classList.add = ("particle");
+        let newParticle = this.element;
         newParticle.style.position = "absolute";
         newParticle.style.top = `${this.positionX}px`;
         newParticle.style.left = `${this.positionY}px`;
         newParticle.style.backgroundColor = this.color;
         newParticle.style.width = this.width;
         newParticle.style.height = this.height;
+        newParticle.style.border = "1px solid black";
+        newParticle.style.borderRadius = "50%";
         document.getElementById("playground").appendChild(newParticle);
     }
 
-    update() {
-        this.velocity += 0.05;
-        this.positionY += this.velocity;
+    update() { 
+        // If not colliding, apply gravity
+        if (!this.isColliding) {
+            this.velocity += gravity;
+            this.positionY += this.velocity;
+        }
 
         const floor = document.getElementById("floor");
         const floorTop = floor.getBoundingClientRect().top;
 
-        if (this.positionY + parseInt(this.height) > floorTop) {
-            this.positionY = floorTop + parseInt(this.height) - 20;
+        if (this.positionY + parseInt(this.height) > floorTop-3.5) {
+            this.positionY = floorTop + parseInt(this.height) - 23.5;
             this.velocity = 0;
         }
     }
@@ -45,6 +56,7 @@ export class Particle {
     }
 
     destroy() {
+        this.setColliding(false);
         let element = this.getElement();
         element.remove();
     }
